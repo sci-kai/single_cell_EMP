3fd74d996d9c47c4cebd652619d7863a88f29ccf
sci-kai <63246519+sci-kai@users.noreply.github.com>
2022-01-13 16:19:07 +0100

    Create cellrangercommand.sh

---
+++ cellrangercommand.sh
@@ -0,0 +1,3 @@
+#Command for running cellranger
+
+/software/cellranger/3.1.0/cellranger-cs/3.1.0/bin/count --id=D19200 --fastqs=/lsdf/dkfz/analysis/L441/scRNA-D19200/fastqs --transcriptome=/lsdf/dkfz/analysis/L441/scRNA-D19200/refdata-cellranger-GRCh38-3.0.0 --localcores=32 --localmem=128 --jobmode=local
